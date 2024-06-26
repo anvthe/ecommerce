935a6f4519bb7d6d80472da22805305eb2c8fcfd
roney <guddorx@gmail.com>
2024-06-26 20:48:04 +0600

    Initial commit

---
+++ src/main/java/rko/ecommarce/app/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package rko.ecommarce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
