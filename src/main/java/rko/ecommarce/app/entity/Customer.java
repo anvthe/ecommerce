a6c1af777cf6f0c7dea530c44e0f8a399389f273
roney <guddorx@gmail.com>
2024-06-27 23:20:03 +0600

    entity create

--- src/main/java/rko/ecommarce/app/entity/Customer.java
+++ src/main/java/rko/ecommarce/app/entity/Customer.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
