43c0c9190ec5e23c2ec24ab4c3e7fafb3afb694b
roney <guddorx@gmail.com>
2024-06-27 23:26:58 +0600

    All Sales Service

---
+++ src/main/java/rko/ecommarce/app/service/SaleService.java
@@ -0,0 +1,18 @@
+package rko.ecommarce.app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import rko.ecommarce.app.repository.SaleRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class SaleService {
+    private final SaleRepository saleRepository;
+
+    public BigDecimal getTotalSaleAmountToday() {
+        return saleRepository.findTotalSaleAmountByDate(LocalDate.now());
+    }
+}
