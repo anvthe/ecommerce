9a706aea619d2963eaafb2b68e883c9a11b76b99
roney <guddorx@gmail.com>
2024-06-28 22:04:56 +0600

    wishlist controller

--- src/main/java/rko/ecommarce/app/controller/WishlistController.java
+++ src/main/java/rko/ecommarce/app/controller/WishlistController.java
@@ -1,6 +1,7 @@
 package rko.ecommarce.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import rko.ecommarce.app.dto.WishlistDTO;
 import rko.ecommarce.app.service.WishlistService;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/wishlist")
@@ -17,6 +19,7 @@ public class WishlistController {
 
     @GetMapping("/{customerId}")
     public ResponseEntity<?> getCustomerWishlist(@PathVariable Long customerId) {
+        log.info("Get Customer Wishlist by customerId: {}", customerId);
         WishlistDTO wishlist = wishlistService.getWishlistByCustomerId(customerId);
         return ResponseEntity.ok(wishlist);
     }
