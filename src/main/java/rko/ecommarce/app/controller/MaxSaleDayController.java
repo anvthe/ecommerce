376e5c0baafc429c43c98dd21c85ba7d1adcd943
roney <guddorx@gmail.com>
2024-06-28 21:46:34 +0600

    max sale day of a certain time range

--- src/main/java/rko/ecommarce/app/controller/MaxSaleDayController.java
+++ src/main/java/rko/ecommarce/app/controller/MaxSaleDayController.java
@@ -1,6 +1,8 @@
 package rko.ecommarce.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,20 +10,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rko.ecommarce.app.dto.MaxSaleDayDTO;
 import rko.ecommarce.app.service.SaleService;
+
 import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/sales")
 public class MaxSaleDayController {
+    private static final Logger log = LogManager.getLogger(MaxSaleDayController.class);
     private final SaleService saleService;
 
     @GetMapping("/max")
     public ResponseEntity<MaxSaleDayDTO> getMaxSaleDay(@RequestParam String start,
                                                        @RequestParam String end) {
+        log.info("Received request to get max sale day between {} and {}", start, end);
         LocalDate startDate = LocalDate.parse(start);
         LocalDate endDate = LocalDate.parse(end);
         LocalDate maxSaleDay = saleService.getMaxSaleDay(startDate, endDate);
+        log.debug("Max sale day between {} and {} is {}", start, end, maxSaleDay);
         return ResponseEntity.ok(new MaxSaleDayDTO(maxSaleDay.toString()));
     }
 }
