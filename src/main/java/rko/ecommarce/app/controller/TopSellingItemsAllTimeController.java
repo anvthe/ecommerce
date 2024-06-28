d8a3bb9e337f6a48f9ee218dc74033cee6ee6edd
roney <guddorx@gmail.com>
2024-06-28 21:56:08 +0600

    top 5 selling items all time

--- src/main/java/rko/ecommarce/app/controller/TopSellingItemsAllTimeController.java
+++ src/main/java/rko/ecommarce/app/controller/TopSellingItemsAllTimeController.java
@@ -1,6 +1,7 @@
 package rko.ecommarce.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import rko.ecommarce.app.dto.TopSellingItemsAllTimeDTO;
 import rko.ecommarce.app.service.SaleService;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/sales")
@@ -18,6 +20,7 @@ public class TopSellingItemsAllTimeController {
 
     @GetMapping("/top/alltime")
     public ResponseEntity<?> getTop5SellingItemsAllTime() {
+        log.info("Get Top 5 Selling Items All Time");
         List<ItemDTO> topSellingItems = saleService.getTop5SellingItemsAllTime();
         return ResponseEntity.ok(new TopSellingItemsAllTimeDTO(topSellingItems));
     }
