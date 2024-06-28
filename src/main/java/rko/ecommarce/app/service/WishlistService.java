99e39353a17f92f829ece60e8eb9620c5ee1a644
roney <guddorx@gmail.com>
2024-06-28 21:41:04 +0600

    wishlist service

--- src/main/java/rko/ecommarce/app/service/WishlistService.java
+++ src/main/java/rko/ecommarce/app/service/WishlistService.java
@@ -1,6 +1,8 @@
 package rko.ecommarce.app.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import rko.ecommarce.app.dto.ItemDTO;
 import rko.ecommarce.app.dto.WishlistDTO;
@@ -13,13 +15,16 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class WishlistService {
+    private static final Logger log = LogManager.getLogger(WishlistService.class);
     private final WishlistRepository wishlistRepository;
 
     public WishlistDTO getWishlistByCustomerId(Long customerId) {
+        log.info("Fetching wishlist for customer with ID: {}", customerId);
         List<Item> items = wishlistRepository.findByCustomerId(customerId);
         List<ItemDTO> itemDTOs = items.stream()
                 .map(ItemDTO::from)
                 .collect(Collectors.toList());
+        log.debug("Wishlist items for customer {}: {}", customerId, itemDTOs);
         return new WishlistDTO(itemDTOs);
     }
 }
