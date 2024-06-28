0dfad6ffc4d7b4f0083800168e24d692d0fbb69c
roney <guddorx@gmail.com>
2024-06-28 21:40:31 +0600

    all sales service

--- src/main/java/rko/ecommarce/app/service/SaleService.java
+++ src/main/java/rko/ecommarce/app/service/SaleService.java
@@ -1,6 +1,8 @@
 package rko.ecommarce.app.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,24 +17,37 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class SaleService {
+    private static final Logger log = LogManager.getLogger(SaleService.class);
     private final SaleRepository saleRepository;
 
     public BigDecimal getTotalSaleAmountToday() {
-        return saleRepository.findTotalSaleAmountByDate(LocalDate.now());
+        log.info("Getting total sale amount for today");
+        BigDecimal totalSaleAmount = saleRepository.findTotalSaleAmountByDate(LocalDate.now());
+        log.debug("Total sale amount for today: {}", totalSaleAmount);
+        return totalSaleAmount;
     }
 
     public LocalDate getMaxSaleDay(LocalDate start, LocalDate end) {
+        log.info("Getting max sale day between {} and {}", start, end);
         Pageable pageable = PageRequest.of(0, 10);
-        return saleRepository.findMaxSaleDayBetween(start, end, pageable);
+        LocalDate maxSaleDay = saleRepository.findMaxSaleDayBetween(start, end, pageable);
+        log.debug("Max sale day: {}", maxSaleDay);
+        return maxSaleDay;
     }
 
     public List<ItemDTO> getTop5SellingItemsAllTime() {
-        return saleRepository.findTop5SellingItemsAllTime(PageRequest.of(0, 5));
+        log.info("Getting top 5 selling items of all time");
+        List<ItemDTO> topSellingItems = saleRepository.findTop5SellingItemsAllTime(PageRequest.of(0, 5));
+        log.debug("Top 5 selling items: {}", topSellingItems);
+        return topSellingItems;
     }
 
     public List<ItemDetailsDTO> getTop5SellingItemsLastMonth() {
+        log.info("Getting top 5 selling items of the last month");
         LocalDate lastMonthStart = LocalDate.now().minusMonths(1).withDayOfMonth(1);
         LocalDate lastMonthEnd = lastMonthStart.withDayOfMonth(lastMonthStart.lengthOfMonth());
-        return saleRepository.findTop5SellingItemsByMonth(lastMonthStart, lastMonthEnd, PageRequest.of(0, 5));
+        List<ItemDetailsDTO> topSellingItems = saleRepository.findTop5SellingItemsByMonth(lastMonthStart, lastMonthEnd, PageRequest.of(0, 5));
+        log.debug("Top 5 selling items of the last month: {}", topSellingItems);
+        return topSellingItems;
     }
 }
