18c51151a153a1522f9d8dac003feaf3659bd9f4
roney <guddorx@gmail.com>
2024-06-28 20:06:32 +0600

    top 5 selling items of the last month

--- src/main/java/rko/ecommarce/app/controller/TopSellingItemsLastMonthController.java
+++ src/main/java/rko/ecommarce/app/controller/TopSellingItemsLastMonthController.java
@@ -1,4 +1,24 @@
 package rko.ecommarce.app.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rko.ecommarce.app.dto.ItemDetailsDTO;
+import rko.ecommarce.app.dto.TopSellingItemsLastMonthDTO;
+import rko.ecommarce.app.service.SaleService;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sales")
 public class TopSellingItemsLastMonthController {
+    private final SaleService saleService;
+
+    @GetMapping("/top/lastmonth")
+    public ResponseEntity<TopSellingItemsLastMonthDTO> getTop5SellingItemsLastMonth() {
+        List<ItemDetailsDTO> topSellingItems = saleService.getTop5SellingItemsLastMonth();
+        return ResponseEntity.ok(new TopSellingItemsLastMonthDTO(topSellingItems));
+    }
 }
