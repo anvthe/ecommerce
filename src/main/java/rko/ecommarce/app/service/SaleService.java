c76e5f817260581c6aa58613e71787e9faeb6971
roney <guddorx@gmail.com>
2024-06-28 20:10:26 +0600

    all sales service

--- src/main/java/rko/ecommarce/app/service/SaleService.java
+++ src/main/java/rko/ecommarce/app/service/SaleService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import rko.ecommarce.app.dto.ItemDTO;
 import rko.ecommarce.app.dto.ItemDetailsDTO;
 import rko.ecommarce.app.repository.SaleRepository;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -19,13 +20,17 @@ public class SaleService {
     public BigDecimal getTotalSaleAmountToday() {
         return saleRepository.findTotalSaleAmountByDate(LocalDate.now());
     }
+
     public LocalDate getMaxSaleDay(LocalDate start, LocalDate end) {
         Pageable pageable = PageRequest.of(0, 10);
         return saleRepository.findMaxSaleDayBetween(start, end, pageable);
     }
-    public List<ItemDTO> getTop5SellingItemsAllTime() {;
+
+    public List<ItemDTO> getTop5SellingItemsAllTime() {
+        ;
         return saleRepository.findTop5SellingItemsAllTime(PageRequest.of(0, 5));
     }
+
     public List<ItemDetailsDTO> getTop5SellingItemsLastMonth() {
         LocalDate lastMonthStart = LocalDate.now().minusMonths(1).withDayOfMonth(1);
         LocalDate lastMonthEnd = lastMonthStart.withDayOfMonth(lastMonthStart.lengthOfMonth());
