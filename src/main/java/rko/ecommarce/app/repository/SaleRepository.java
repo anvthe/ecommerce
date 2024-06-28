8764cfa760b9647750a0cb63a856a2c4102361e4
roney <guddorx@gmail.com>
2024-06-28 23:31:57 +0600

    all repository

--- src/main/java/rko/ecommarce/app/repository/SaleRepository.java
+++ src/main/java/rko/ecommarce/app/repository/SaleRepository.java
@@ -19,7 +19,7 @@ public interface SaleRepository extends CrudRepository<Sale, Long> {
     BigDecimal findTotalSaleAmountByDate(@Param("date") LocalDate date);
 
     @Query("SELECT s.date, SUM(s.amount) AS total FROM Sale s WHERE s.date BETWEEN :start AND :end GROUP BY s.date ORDER BY total DESC LIMIT 1")
-    LocalDate findMaxSaleDayBetween(@Param("start") LocalDate start, @Param("end") LocalDate end, Pageable pageable);
+    List<LocalDate> findMaxSaleDayBetween(@Param("start") LocalDate start, @Param("end") LocalDate end, Pageable pageable);
 
     @Query("SELECT new rko.ecommarce.app.dto.ItemDTO(i.id, i.name, SUM(s.amount)) FROM Sale s JOIN s.item i GROUP BY i.id, i.name ORDER BY SUM(s.amount) DESC")
     List<ItemDTO> findTop5SellingItemsAllTime(Pageable pageable);
