be27b1eaf307f907ce8a15d8f84405ea9acf0f71
roney <guddorx@gmail.com>
2024-06-28 22:01:31 +0600

    total sale amount current day

--- src/main/java/rko/ecommarce/app/controller/TotalSaleAmountCurrentDayController.java
+++ src/main/java/rko/ecommarce/app/controller/TotalSaleAmountCurrentDayController.java
@@ -1,6 +1,7 @@
 package rko.ecommarce.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import rko.ecommarce.app.dto.TotalSaleAmountDTO;
 import rko.ecommarce.app.service.SaleService;
 import java.math.BigDecimal;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/sales")
@@ -17,6 +19,7 @@ public class TotalSaleAmountCurrentDayController {
 
     @GetMapping("/today")
     public ResponseEntity<?> getTotalSaleAmountToday() {
+        log.info("Total sale amount current day");
         BigDecimal totalSaleAmount = saleService.getTotalSaleAmountToday();
         return ResponseEntity.ok(new TotalSaleAmountDTO(totalSaleAmount));
     }
