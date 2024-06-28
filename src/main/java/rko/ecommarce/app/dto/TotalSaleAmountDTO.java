e8694f3b72f1b4a7cdaa390d57d30474237e9c88
roney <guddorx@gmail.com>
2024-06-28 19:53:50 +0600

    dto create

--- src/main/java/rko/ecommarce/app/dto/TotalSaleAmountDTO.java
+++ src/main/java/rko/ecommarce/app/dto/TotalSaleAmountDTO.java
@@ -3,8 +3,8 @@ package rko.ecommarce.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/rko/ecommarce/app/dto/WishlistDTO.java
+++ src/main/java/rko/ecommarce/app/dto/WishlistDTO.java
@@ -3,7 +3,6 @@ package rko.ecommarce.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @Data
